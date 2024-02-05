0332726a19650f1448a7406017912ebeea1818ed
Deestad <kalliddel@gmail.com>
2024-02-05 00:15:49 -0300

    WIP, work started building the interface. Prototype in development in Figma.

--- searcher.py
+++ searcher.py
@@ -1,40 +1,37 @@
 import sys
 import pickle
-from _ast import Lambda
-
-from selenium import webdriver
 import time
+import tkinter as tk
+import webbrowser
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.remote.webelement import WebElement
-import tkinter as tk
-from tkinter import ttk
-from tkinter import *
-import webbrowser
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 options = webdriver.FirefoxOptions()
 options.add_argument("-headless")
+
 app = tk.Tk()
 app.title('Ferramenta de Busca - β1')
 app.geometry("800x500")
 app.iconbitmap("FaviconAlt3.ico")
+
 Googling = False
+
 IQ = tk.Entry(app, width=100, borderwidth=15, relief=tk.FLAT, bg="lightgrey")
 IQ.pack()
 IQ.insert(0, "O que você está procurando?")
 
-AuthorCopy = Label(app, text="Lys Kaldwin 2024 © Todos os direitos reservados.")
+AuthorCopy = tk.Label(app, text="Lys Kaldwin 2024 © Todos os direitos reservados.")
 AuthorCopy.pack()
-L1 = Listbox(app)
 
+L1 = tk.Listbox(app)
 
 def InitialQuestion():
     L1.delete(0, tk.END)
 
     browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
-
     browser.get("https://www.google.com")
     pickle.dump(browser.get_cookies(), open("cookies.pkl", "wb"))
 
@@ -46,18 +43,16 @@ def InitialQuestion():
 
     browser.find_element(By.NAME, "q").send_keys(IQ.get())
     time.sleep(5)
-    browser.find_element(By.NAME,"q").send_keys(Keys.ENTER)
+    browser.find_element(By.NAME, "q").send_keys(Keys.ENTER)
     results_list = browser.find_elements(By.XPATH, '//div[@class="g"]//a')
     i = 0
 
     def OpenURL(event):
-        weblink = L1.get(ACTIVE)
+        weblink = L1.get(tk.ACTIVE)
         webbrowser.open(weblink)
 
     for item in results_list:
-
-        Attribute = [item.get_attribute('href') for results_list in
-                     browser.find_elements(By.XPATH, '//div[@class="yuRUbf"]/a')]
+        Attribute = [item.get_attribute('href') for item in browser.find_elements(By.XPATH, '//div[@class="yuRUbf"]/a')]
 
         for items in Attribute:
             L1.insert(i, items)
@@ -65,36 +60,24 @@ def InitialQuestion():
 
         if i == 100:
             browser.find_element(By.ID, "pnnext").click()
+            results_list = browser.find_elements(By.XPATH, '//div[@class="g"]//a')
 
             for item in results_list:
-
-                Attribute = [item.get_attribute('href') for results_list in
-                             browser.find_elements(By.XPATH,'//div[@class="yuRUbf"]/a')]
+                Attribute = [item.get_attribute('href') for item in browser.find_elements(By.XPATH, '//div[@class="yuRUbf"]/a')]
 
                 for items in Attribute:
                     L1.insert(i, items)
-
                     i += 1
 
                 if i == 100:
-                    for item in results_list:
-                        Attribute = [item.get_attribute('href') for results_list in
-                                     browser.find_elements(By.XPATH,'//div[@class="yuRUbf"]/a')]
-
-                    for items in Attribute:
-                        L1.insert(i, items)
-
-                        i += 1
-
-                    if i == 100:
-                        break
+                    results_list = browser.find_elements(By.XPATH, '//div[@class="g"]//a')
                     break
             break
 
     browser.quit()
 
     L1.bind("<Double-Button-1>", OpenURL)
-    L1.pack(expand=True, fill=BOTH)
+    L1.pack(expand=True, fill=tk.BOTH)
 
 
 Button = tk.Button(app, text="Enviar", padx=40, pady=2, command=InitialQuestion, bg='lightgreen')
