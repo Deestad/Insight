260977f14b8ebcff8d6e0e6030502aaeec2ad16a
Deestad <kalliddel@gmail.com>
2024-02-05 21:51:27 -0300

    Button functionality WIP

--- main.py
+++ main.py
@@ -3,13 +3,17 @@
 import webbrowser
 import sys
 import os
+import logging
+import requests
 import winsound
-# Third-party imports
 
+# Third-party imports
+from bs4 import BeautifulSoup
 # -- Kivy
 import kivy
 from kivy.app import App
 from kivy.config import Config
+
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '600')
 Config.set('graphics', 'height', '400')
@@ -34,18 +38,47 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
+# Logging
+LOG_FILE = 'Insight.log'
+if os.path.isfile(LOG_FILE) and os.access(LOG_FILE, os.R_OK):
+    os.remove('Insight.log')
+logging.basicConfig(filename='Insight.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 options = webdriver.FirefoxOptions()
 options.add_argument("-headless")
 
+
 class MainScreen(TabbedPanel):
     pass
+
 
 class InsightApp(App):
     def build(self):
         self.title = 'Insight: Internet Browsing Optimizer'
         return MainScreen()
 
+    def search_callback(self):
+        logging.info(f"[Search Callback Function          ] Running...")
+        query = self.root.ids.searchbar.text
+        query = query.replace(' ', '+')
+        logging.info(f"[Search Callback Function          ] Query set as {query}...")
+        if query:
+            with requests.session() as s:
+                url = f"https://www.google.com/search?q=`{query}`"
+                headers = {
+                    "referer": "referer: https://www.google.com/",
+                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                  "Chrome/89.0.4389.114 Safari/537.36"
+                }
+                s.post(url, headers=headers)
+                response = s.get(url, headers=headers)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                titles = soup.findAll('h3')
+                titles_url = [element for element in soup.findAll('a', href=True)]
+                titles = [element.get_text() for element in titles]
+                for i in range(0, len(titles)-2):
+                    print(titles[i], titles_url[i]['href'])
+
 
 if __name__ == '__main__':
     InsightApp().run()
-
